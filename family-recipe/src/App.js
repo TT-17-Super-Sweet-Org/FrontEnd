fd8b24a0bd7d339f4c0f4a1d73d080d59ace7ea4
L-Steinmacher <dasai.sacul@gmail.com>
2021-03-01 10:06:17 -0800

    boilerplate setup

--- family-recipe/src/App.js
+++ family-recipe/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useState} from 'react'
+import * as yup from 'yup'
+import axios from 'axios'
+import { Switch, Link, Route } from 'react-router-dom'
+import Home from './components/Home'
+import Login from './components/Login'
+import RecipeInput from './components/RecipeInput'
+
+
+const initialRecipe ={
+  title: '',
+  source: '',
+  ingredients: '',
+  instructions: '',
+  category: '',
+}
+
+
 
 function App() {
+
+  const [recipe, setRecipe] = useState(initialRecipe)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1 className='myheader'>Super Sweet Recipe Page!</h1>
+        <div className='navLinks'>
+          <Link to='/'>Login</Link>
+          <Link to='/home'>Home</Link>
+          <Link to='/recipes'>Add Recipe</Link>
+        </div>
+      </nav>
+
+      <Switch>
+        <Route path='/recipes'>
+          <RecipeInput recipe={recipe} setRecipe={setRecipe} />
+        </Route>
+        
+        <Route path='/home'>
+          <Home />
+        </Route>
+
+        <Route path='/'>
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ family-recipe/src/components/RecipeInput.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+
+
+
+export default function AddRecipe(props) {
+
+    const {recipe, setRecipe} = props
+
+    const changeHandler = e =>{
+        setRecipe({...recipe, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault()
+    }
+
+    return (
+        <div>
+            <h2>Recipe Imputs</h2>
+            <form onSubmit={handleSubmit}>
+                <input
+                type= 'text'
+                name= 'title'
+                // value= {recipe.title}
+                // placeholder= 'Title'
+                // onChange= {changeHandler}
+                />
+                <input
+                type= 'text'
+                name= 'source'
+                // value= {recipe.source}
+                // placeholder= 'Source ex: Grandma, Mom'
+                // onChange= {changeHandler}
+                />
+                <input
+                type= 'text'
+                name= 'ingredients'
+                // value= {recipe.ingredients}
+                // placeholder= 'Ingredients'
+                // onChange= {changeHandler}
+                />
+                <input
+                type= 'text'
+                name= 'instructions'
+                // value= {recipe.instructions}
+                // placeholder= 'Instructions'
+                // onChange= {changeHandler}
+                />
+                <input
+                type= 'text'
+                name= 'category'
+                // value= {recipe.category}
+                // placeholder= 'Category'
+                // onChange= {changeHandler}
+                />
+            </form>
+        </div>
+    )
+}
